4f849fd9dc580318c4b9915ed1a9c0c40760b3f0
shakil <shakil.aktar@nineleaps.com>
2023-03-24 15:34:56 +0530

    commit

--- cypress/inetrgration/example/demo11.js
+++ cypress/inetrgration/example/demo11.js
@@ -2,7 +2,7 @@ describe('drag and drop', () => {
     it('drag', () => {
         cy.visit('https://simple-drag-drop.glitch.me/')
         // cy.contains('A').trigger('mousedown')
-        cy.wait(set)
+        // cy.wait(set)
         // cy.contains("B").trigger('mousemove').trigger('mouseup')
         cy.get('.box').eq(0).trigger('mousedown')
         cy.get('.box').eq(1).trigger('mousemove').trigger('mouseup')
