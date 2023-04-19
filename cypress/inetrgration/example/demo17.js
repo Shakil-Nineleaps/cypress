628ab621a70c1adb4e6be7333923537c3426d6e3
shakil <shakil.aktar@nineleaps.com>
2023-04-19 18:17:50 +0530

    commit

---
+++ cypress/inetrgration/example/demo17.js
@@ -0,0 +1,6 @@
+describe('', () => {
+    it('', () => {
+       cy.visit("https://pda-stage.socion.io/new/app/profile/additionalProfileInfo") 
+       cy.get(':nth-child(6) > #customScrollBarId > :nth-child(1) > :nth-child(2) > :nth-child(1) > [data-testid="chk"] > [data-testid="to"] > .css-1dbjc4n > .css-901oao')
+    });
+});
